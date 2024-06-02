9dc8ecf4a5d2f68f419d8cdc86e418dd7bacf088
Álvaro García <alvgarcia@cftla.cl>
2024-06-02 13:11:57 -0400

    login funcional

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,4 @@
 "use client";
-
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
@@ -11,62 +7,15 @@ import { Authenticator } from "@aws-amplify/ui-react";
 import "../components/translations";
 Amplify.configure(outputs);
 
-const client = generateClient<Schema>();
-
 export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
-
-  useEffect(() => {
-    listTodos();
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
-
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({id});
-  }
   return (
-    <Authenticator signUpAttributes={[
-
-      'email',
-      'family_name',
-      'middle_name',
-      'name',
-    ]}>
-      {({signOut,user}) =>(
+    <Authenticator signUpAttributes={["email","name","middle_name", "family_name" ]}>
+      {({ signOut }) => (
         <main>
-        <h1>My todos</h1>
-        <button onClick={createTodo}>+ new</button>
-        <ul>
-          {todos.map((todo) => (
-            <li key={todo.id}
-            onClick={() => deleteTodo(todo.id)}
-            >{todo.content}</li>
-          ))}
-        </ul>
-        <button
-        onClick={signOut}>Cerrar sesión</button>
-        <div>
-          🥳 App successfully hosted. Try creating a new todo.
-          <br />
-          <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-            Review next steps of this tutorial.
-          </a>
-        </div>
-        
-      </main>
-    )}
-    
+          <h1>Iniciar sesión</h1>
+          <button onClick={signOut}>Cerrar sesión</button>
+        </main>
+      )}
     </Authenticator>
   );
 }
